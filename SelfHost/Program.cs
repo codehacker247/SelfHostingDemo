70abea797360f96b82daba846f83ab005405ea27
Rohit <rohitkumarmandal34@gmail.com>
2022-12-28 19:29:27 +0530

    Add project files.

---
+++ SelfHost/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathServiceLibrary;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace SelfHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var servicehost = new ServiceHost(typeof(MathService)))
+            {
+                ServiceEndpoint BasicHttpEndPoint1 = servicehost.AddServiceEndpoint(
+                    typeof(IMathService),
+                    new BasicHttpBinding(),
+                    "http://localhost:4444/MathService");
+
+                ServiceEndpoint BasicHttpEndPoint2 = servicehost.AddServiceEndpoint(
+                    typeof(IMathService),
+                    new BasicHttpBinding(),
+                    "http://localhost:5555/MathService");
+
+                ServiceEndpoint NettcpEndPoint = servicehost.AddServiceEndpoint(
+                    typeof(IMathService),
+                    new NetTcpBinding(),
+                    "net.tcp://localhost:6666/MathService");
+
+                servicehost.Open();
+
+                Console.WriteLine("Your WCF Service is running");
+                Console.WriteLine("Your WCF Math Service is running is listening on below endpoints");
+
+                foreach(ServiceEndpoint endpoint in servicehost.Description.Endpoints)
+                {
+                    Console.WriteLine("Address : {0} Binding Name : {1}",endpoint.Address.ToString(),endpoint.Binding.Name);
+                }
+                Console.WriteLine("Press any key to stop your WCF Math Service.");
+                Console.ReadKey();  
+
+                servicehost.Close();    
+            }
+
+        }
+    }
+}
---
+++ WindowsClient1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathServiceLibrary;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+namespace WindowsClient1
+{
+    public partial class Form1 : Form
+    {
+        public IMathService channel = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            var endpoint = new EndpointAddress("http://localhost:4444/MathService");
+
+            channel = ChannelFactory<IMathService>.CreateChannel(new BasicHttpBinding(), endpoint);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyNumbers obj = new MyNumbers();
+            obj.Number1 = Convert.ToInt32(textBox1.Text);
+            obj.Number2 = Convert.ToInt32(textBox2.Text);    
+            textBox3.Text= channel.Add(obj).ToString(); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MyNumbers obj = new MyNumbers();
+            obj.Number1 = Convert.ToInt32(textBox1.Text);
+            obj.Number2 = Convert.ToInt32(textBox2.Text);
+            textBox3.Text = channel.Subtract(obj).ToString();
+        }
+    }
+}
